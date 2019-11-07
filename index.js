21351d5c7bed39acbb9c59f70bebfd019440a071
Andrew Harper <andrewjharper01@gmail.com>
2019-11-07 12:02:24 -0600

    changes made during practice

---
+++ index.js
@@ -0,0 +1,18 @@
+const name = "Andrew Harper";
+
+// Template Literal
+console.log(`Hello World! This is ${name}`);
+
+// Object Literal
+const me = {
+  name:"Drew",
+  age: 28,
+  weight: 190,
+  status: "student",
+};
+
+// We are using bracket notation
+console.log(me["name"]);
+
+// We are using dot notation
+console.log(me.age)
