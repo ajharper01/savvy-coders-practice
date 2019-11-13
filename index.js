c2099daf74c6c79c761ea73f0d7d9b0a6f361db8
Andrew Harper <andrewjharper01@gmail.com>
2019-11-12 20:06:23 -0600

    function factory

--- index.js
+++ index.js
@@ -1,39 +1,10 @@
-function doMath(x, y, operation){
-  console.trace();
-  return operation(x,y)}
+//Next Lesson FUNCTION FACTORY
 
-function oepration(x, y){
-  console.trace();
+function initSportsScorer(pts){
+  return function(score){
+    return score + pts;
+  };
 
-  return x + y;
-}
-
-console.log(doMath(operation));
-
-function doMath(x, y, addition){
-  console.trace();
-
-  return addition(x,y)}
-
-function add(x, y){
-  console.trace();
-
-  return x + y;
-}
-
-console.log(doMath(addition));
-
-function doMath(x, y, subtract){
-  console.trace();
-
-  return subtract(x,y)}
-
-function subtract(x, y){
-  console.trace();
-
-  return x - y;
-}
-
-console.log(doMath(subtract));
-
-
+  const threePointer = initSportsScorer(3);
+  console.log(threePointer);
+  }
