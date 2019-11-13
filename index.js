ce182a83d8b177b4f78b9b12bd944eec84140a44
Andrew Harper <andrewjharper01@gmail.com>
2019-11-12 20:14:09 -0600

    fixes to file

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ function initSportsScorer(pts){
   return function(score){
     return score + pts;
   };
-
+}
   const threePointers = initSportsScorer(3);
+
   const touchdown = initSportsScorer(7);
+
   console.log(threePointers(99));
   console.log(touchdown(14));
-  };
-
   /**
    * function (score){
    * return score + 3;
