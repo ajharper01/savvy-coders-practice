34cca6d0001879985827513a42b1fe017134ef75
Andrew Harper <andrewjharper01@gmail.com>
2019-11-12 20:04:34 -0600

    function within functions

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-function doMath(x, y, mathFxn){
-  return mathFxn(x,y)}
+function doMath(x, y, operation){
+  console.trace();
+  return operation(x,y)}
 
-function add(x, y){
+function oepration(x, y){
+  console.trace();
+
   return x + y;
 }
 
-console.log(doMath(add));
+console.log(doMath(operation));
+
+function doMath(x, y, addition){
+  console.trace();
+
+  return addition(x,y)}
+
+function add(x, y){
+  console.trace();
+
+  return x + y;
+}
+
+console.log(doMath(addition));
+
+function doMath(x, y, subtract){
+  console.trace();
+
+  return subtract(x,y)}
+
+function subtract(x, y){
+  console.trace();
+
+  return x - y;
+}
+
+console.log(doMath(subtract));
+
+
