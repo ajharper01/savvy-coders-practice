951ceea257c8f259710f8fa0d8bc23a40b7d37dc
Andrew Harper <andrewjharper01@gmail.com>
2019-11-14 20:16:12 -0600

    recursion example added in

--- index.js
+++ index.js
@@ -8,4 +8,16 @@ function myPow(num, exp) {
 }
 console.log(myPow(2, 3));
 
-//num can be any number you want to start with to multiply. The exp is for exponents, how many times you want the num to be multiplied by. This is a for Loop. be sure to call the function with Console.log
+//the above is a for loop example...num can be any number you want to start with to multiply. The exp is for exponents, how many times you want the num to be multiplied by. This is a for Loop. be sure to call the function with Console.log
+
+function myPow(num, exp) {
+if (exp === 0){
+return 1;
+}
+
+return (num *= myPow(num, exp - 1));
+};
+
+console.log(myPow(2,3));
+
+//this is the same thing just using recursion
