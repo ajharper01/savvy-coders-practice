fda554dd5ee4e70c7d24a4d29b47f8e933ef3d50
Andrew Harper <andrewjharper01@gmail.com>
2019-11-18 20:50:41 -0600

    this is what you do not do

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-function initFizzBuzz(num1, num2) {
-  return function fizzBuzz46() {
-    for (let i = 0; i <= 99; i += 1) {
-      //you could also do[if (i % (num1 * num2) = = = 0)]
-      if (i % num1 === 0 && i % num2 === 0) {
-        console.log(`${i} - fizzbuzz`);
-        //you could also (!(i% num1)) if you choose to
-      } else if (i % num1 === 0) {
-        console.log(`${i} - fizz`);
-      } else if (i % num2 === 0) {
-        console.log(`${i} - buzz`);
-      } else {
-        console.log(i);
-      }
-    }
+const mySelf = `The Son`;
+
+function initFamily(birthFather, birthMother, mySelf) {
+  return {
+    birthFather,
+    birthMother,
+    mySelfParam: mySelf,
+    getBirthFather: () => `Ron Harper is my: ${this.birthFather}.`,
+    //with arrow syntax the use of this.birthFather is useless
+    //Do not use THIS in an ARROW SYNTAX
+    getBirthMother: function() {
+      return `Janice Johnson is my ${this.birthMother}.`
+    },
+    getMySelf: () =>
+      `They made me, ${mySelfParam}.`
   };
 }
-const fizzBuzz46 = initFizzBuzz(4, 6);
-fizzBuzz46();
+const myFamily = initFamily("father","mother","son");
 
-const bob = [];
-console.log(Boolean(null)); //false
-console.log(Boolean(undefined)); //false
-console.log(Boolean(bob)); //true
-//! is Something or Nothing only when it comes to primitives//
-//DOES NOT APPLY TO COLLECTIONS
+console.log(myFamily);
+
+console.log(myFamily.getBirthFather());
+console.log(myFamily.getBirthMother);
+console.log(myFamily.getMySelf);
+//THIS IS WHAT YOU DO NOT DO
