0cfefd4c08e1b964725fc7f6dd0e928e81cc863f
Andrew Harper <andrewjharper01@gmail.com>
2019-11-18 18:56:01 -0600

    made fizzbuzz better

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ function initFizzBuzz(num1, num2) {
   return function fizzBuzz46() {
     for (let i = 0; i <= 99; i += 1) {
       if (i % num1 === 0 && i % num2 === 0) {
-        console.log(`fizzbuzz`);
+        console.log(`${i} - fizzbuzz`);
       } else if (i % num1 === 0) {
-        console.log(`fizz`);
+        console.log(`${i} - fizz`);
       } else if (i % num2 === 0) {
-        console.log(`buzz`);
+        console.log(`${i} - buzz`);
       } else {
         console.log(i);
       }
