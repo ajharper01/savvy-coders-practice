37e0b7694d1935dc4cc0656f08c75f8f3a08182a
Andrew Harper <andrewjharper01@gmail.com>
2019-11-18 19:27:55 -0600

    added examples of not

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function initFizzBuzz(num1, num2) {
   return function fizzBuzz46() {
     for (let i = 0; i <= 99; i += 1) {
+      //you could also do[if (i % (num1 * num2) = = = 0)]
       if (i % num1 === 0 && i % num2 === 0) {
         console.log(`${i} - fizzbuzz`);
+        //you could also (!(i% num1)) if you choose to
       } else if (i % num1 === 0) {
         console.log(`${i} - fizz`);
       } else if (i % num2 === 0) {
@@ -15,3 +17,8 @@ function initFizzBuzz(num1, num2) {
 }
 const fizzBuzz46 = initFizzBuzz(4, 6);
 fizzBuzz46();
+
+const bob = [];
+console.log(Boolean(null)); //false
+console.log(Boolean(undefined)); //false
+console.log(Boolean(bob)); //true
