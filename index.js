fa1b4f9e40095cc8aae93ed792358f265d02fc26
Andrew Harper <andrewjharper01@gmail.com>
2019-11-19 19:58:50 -0600

    getters,setters, typeof operator debugging practice

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
-function Menu(entree, drink, side) {
-  this.entree = entree;
-  this.drink = drink;
-  this.side = side;
-  this.mealForToday = function() {
-    if (this.meal === "soda") {
-      return `This is my favorite drink ${this.drink}.`;
+//FUNCTION CONSTRUCTOR
+function WolfTrainer(breed, age, eyeColor) {
+  this.breed = breed;
+  this.age = age;
+  this.eyeColor = eyeColor;
+  this.ageLimiter = function() {
+    if (this.age <= 1) {
+      return "Do not pull cub from pack for training, it is too young!";
+    } else {
+      return "The chosen cub is old enough to be removed from the pack and receive training.";
     }
-      else {
-        return `It is not the healthiest choice but I like it.`;}
-    };
-  this.orderFood = function() {return `Hello, I'd like to get ${this.entree}. I would also like ${this.drink}`
+  };
+  this.wolfBreed = function() {
+    return `${this.breed} Wolf`;
+  };
+  this.myProfile = function() {
+    return `This guidance comes from Andrew, the ${this.wolfBreed()} Whisperer. He is ${
+      this.age
+    } years old and has ${this.eyeColor} eyes.`;
+  };
+  this.setMyNewBreed = function(myNewBreed) {
+    if (!myNewBreed) {
+      console.error("No new breeds allowed per state law 17.973.");
+      return;
+    }
+    this.breed = "White";
+  };
+  this.setNewTrainer = function(NewTrainer) {
+    if (NewTrainer && typeof NewTrainer === "string") {
+      this.NewTrainer = "Alfred";
+    } else {
+      console.error("All trainers must be qualified!");
+    }
   };
 }
-const me = new Menu("breakfast", "soda", "fries");
-console.log(me);
-console.log(me.mealForToday());
-console.log(me.orderFood());
+const trainer = new WolfTrainer("Grey", 28, "Honey Brown");
+console.log(trainer);
+console.log(trainer.ageLimiter());
+console.log(trainer.myProfile());
+console.log(trainer.wolfBreed());
+
+//setter example
+trainer.setNewTrainer("Alfred");
+trainer.setMyNewBreed("White");
+console.log("New Breed", trainer);
