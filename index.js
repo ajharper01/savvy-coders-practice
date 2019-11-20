09175d71f655ee4b014cdfcd1b2cd5060aff136d
Andrew Harper <andrewjharper01@gmail.com>
2019-11-19 18:47:00 -0600

    Nikki's Fix

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
-//FUNCTION CONSTRUCTOR
-function Car(year, make, model, trans){
-	this.year = year;
-	this.make = make;
-	this.model = model;
-	this.trans = trans;
-	this.carShow = function (){
-		return `This is a ${this.year}, ${this.make}. It has a ${this.trans} transmission.`
-	};
-	this.isBeater = function() {
-		if(this.year <= 2005) {
-			return `It's 2019 man! You need a new car!`;
-		} else {
-			return "This is a sparkly new whip! ";
-		}
-	};
-	this.isStick = function() {
-		if (this.trans === "Manual"){
-			return 	`Don't forget to practice your ${this.trans} hill starts!`
-		}
-		else {
-			return `You have nothing to worry about, you drive a ${this.trans}..`
-		}
-	}
-};
-
-const fresh = new Car(2017, "Subaru", "WRX", "Manual");
-console.log(fresh.carShow());
-console.log(fresh.isBeater());
-console.log(fresh.isStick());
+function Menu(entree, drink, side) {
+  this.entree = entree;
+  this.drink = drink;
+  this.side = side;
+  this.mealForToday = function() {
+    if (this.meal === "soda") {
+      return `This is my favorite drink ${this.drink}.`;
+    }
+      else {
+        return `It is not the healthiest choice but I like it.`;}
+    };
+  this.orderFood = function() {return `Hello, I'd like to get ${this.entree}. I would also like ${this.drink}`
+  };
+}
+const me = new Menu("breakfast", "soda", "fries");
+console.log(me);
+console.log(me.mealForToday());
+console.log(me.orderFood());
