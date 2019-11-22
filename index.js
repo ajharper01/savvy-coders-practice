10433185ee585493f3c4f13da368a8f7956f7ebb
Andrew Harper <andrewjharper01@gmail.com>
2019-11-21 21:20:22 -0600

    class get and set examples

--- index.js
+++ index.js
@@ -1,11 +1,114 @@
-//HW6 Solution
+//FUNCTION CONSTRUCTOR
+//function WolfTrainer(breed, age, eyeColor) {
+// this.breed = breed;
+//this.age = age;
+//this.eyeColor = eyeColor;
+//}
 
-function WolfTrainer(name, age, eyeColor) {
-  this.name = name;
-  this.age = age;
-  this.eyeColor = eyeColor;
+//WolfTrainer.prototype.ageLimiter = function() {
+//if (this.age <= 1) {
+//return "Do not pull cub from pack for training, it is too young!";
+//} else {
+// return "The chosen cub is old enough to be removed from the pack and receive training.";
+//}
+//};
+
+//WolfTrainer.prototype.wolfBreed = function() {
+//return `${this.breed} Wolf`;
+//};
+
+//WolfTrainer.prototype.myProfile = function() {
+//return `This guidance comes from Andrew, the ${this.wolfBreed()} Whisperer. He is ${
+//this.age
+//} years old and has ${this.eyeColor} eyes.`;
+//};
+
+//WolfTrainer.prototype.setMyNewBreed = function(myNewBreed) {
+//if (!myNewBreed) {
+//console.error("No new breeds allowed per state law 17.973.");
+//return;
+//}
+//this.breed = "White";
+//};
+
+//WolfTrainer.prototype.setNewTrainer = function(NewTrainer) {
+//if (NewTrainer && typeof NewTrainer === "string") {
+//  this.NewTrainer = "Alfred";
+//} else {
+//  console.error("All trainers must be qualified!");
+//}
+//newBreeds.wolfBreed = function() {
+//  return "New breeds need to have papers.";
+//};
+//};
+//const newBreeds = new WolfTrainer(["Timber", "Red", "Long-hair"]);
+//const trainer = new WolfTrainer("Grey", 28, "Honey Brown");
+
+//trainer.setNewTrainer("Alfred");
+//trainer.setMyNewBreed("White");
+
+//const newTraits = [
+//`Hair: Golden Brown`,
+//`10 Fingers`,
+//`10 Toes`,
+//`Height: 69 Inches`,
+//`Weight 190`
+//];
+
+//WolfTrainer.prototype.setNewTraits = function(myArray) {
+//if (Array.isArray(myArray)) {
+//  this.newTraits = myArray;
+//} else {
+// console.error("Andrew's Traits Not Loaded");
+//}
+//};
+
+function trainingCenter(city, state) {
+  this.city = city;
+  this.state = state;
 }
 
+function trainingLocation(city, state) {
+  trainingCenter.call(this, city, state);
+  this.trainingType = "Basic Commands";
+}
+
+const locationData = new trainingLocation("St. Louis", "Missouri");
+console.log(locationData);
+
+//CLASS DOES NOT CHANGE THE FACT THAT WE ARE DOING PROTOTYPE OOP AND NOT CLASS-BASED OOP!
+//We will use CLASS to implement function constructor prototypes
+
+class WolfTrainer {
+  constructor(breed, age, eyeColor) {
+    this.breed = breed;
+    this.age = age;
+    this.eyeColor = eyeColor;
+  }
+
+  get ageLimiter() {
+    if (this.age <= 1) {
+      return "Do not pull cub from pack for training, it is too young!";
+    } else {
+      return "The chosen cub is old enough to be removed from the pack and receive training.";
+    }
+  }
+  get wolfBreed() {
+    return `${this.breed} Wolf`;
+  }
+  get myProfile() {
+    return `This guidance comes from Andrew, the ${this.wolfBreed()} Whisperer. He is ${
+      this.age
+    } years old and has ${this.eyeColor} eyes.`;
+  }
+  set changeBreed(newBreed) {
+    if (newBreed) {
+      this.breed = newBreed;
+    } else {
+      console.error("This is not a Black Canadian Wolf");
+    }
+  }
+}
 const newTraits = [
   `Hair: Golden Brown`,
   `10 Fingers`,
@@ -14,19 +117,9 @@ const newTraits = [
   `Weight 190`
 ];
 
-WolfTrainer.prototype.setNewTraits = function(myArray) {
-  if (Array.isArray(myArray)) {
-    this.newTraits = myArray;
-  } else {
-    console.error("Andrew's Traits Not Loaded");
-  }
-};
-//Here is my Loop
-for (let i = 0; i <= newTraits.length; i += 1) {
-  console.log(newTraits[i]);
-}
-const myButStatement = `I do not want the terminal knowing my weight, therefor, you must pop it.`;
-newTraits.pop();
-console.log(myButStatement);
-console.log(newTraits);
-console.log(WolfTrainer.newTraits);
+const trainer = new WolfTrainer("Grey", 28, "Honey Brown");
+const trainee = new WolfTrainer("Blue", 2, "yellow");
+console.log(trainer.ageLimiter);
+console.log(trainee);
+console.log((trainee.changeBreed = "black"));
+console.log(trainee);
