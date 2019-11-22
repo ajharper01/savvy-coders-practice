f7b25865039486bb10f1c1103ea097a2b01eaf4f
Andrew Harper <andrewjharper01@gmail.com>
2019-11-21 19:11:04 -0600

    hw6 solution

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-const myArr = new Array();
-console.log(myArr);
+//HW6 Solution
 
-const someArr = [];
-//empty array
+function WolfTrainer(name, age, eyeColor) {
+  this.name = name;
+  this.age = age;
+  this.eyeColor = eyeColor;
+}
 
-/**To cause havoc someArr.myFind = function(){
-  return " I won't find";
-  };
-  console.log(someArr.find());**/
+const newTraits = [
+  `Hair: Golden Brown`,
+  `10 Fingers`,
+  `10 Toes`,
+  `Height: 69 Inches`,
+  `Weight 190`
+];
 
-//See Arrays Concepts
+WolfTrainer.prototype.setNewTraits = function(myArray) {
+  if (Array.isArray(myArray)) {
+    this.newTraits = myArray;
+  } else {
+    console.error("Andrew's Traits Not Loaded");
+  }
+};
+//Here is my Loop
+for (let i = 0; i <= newTraits.length; i += 1) {
+  console.log(newTraits[i]);
+}
+const myButStatement = `I do not want the terminal knowing my weight, therefor, you must pop it.`;
+newTraits.pop();
+console.log(myButStatement);
+console.log(newTraits);
+console.log(WolfTrainer.newTraits);
