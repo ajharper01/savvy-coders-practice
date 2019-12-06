9cb8022565319baef2c4924e7414ccb023ebfec1
Andrew Harper <andrewjharper01@gmail.com>
2019-12-05 19:53:10 -0600

    mapping with numbers

--- playground.js
+++ playground.js
@@ -1,5 +1,20 @@
-const names = ["Nikki", "Drew", "Wallat"];
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const sum = names.reduce((name, accumulator) => (accumulator += name));
-console.log(sum);
-//one line of code to add the array of number 1 through 10 using reduce
+function add1(n) {
+  return n + 1;
+}
+
+function double(n) {
+  return n * 2;
+}
+
+function add1AndDouble(n) {
+  return double(add1(n));
+}
+
+function doubleAndAdd1(n) {
+  return add1(double(n));
+}
+//TODO: Add 1 and double each num inside of nums
+const numbsAddedOneAndDoubled = nums.map(wiggidy => add1AndDouble(wiggidy));
+console.log(numbsAddedOneAndDoubled);
