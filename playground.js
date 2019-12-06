25119f8d238eac3f29ff701c4bc36c3ce46ce6a7
Andrew Harper <andrewjharper01@gmail.com>
2019-12-05 20:54:17 -0600

    reduce example

--- playground.js
+++ playground.js
@@ -1,22 +1,15 @@
-//function composition
+//reduce
 const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-function add1(n) {
-  return n + 1;
+//TODO: Use reduce to sum up these numbers.
+const sumOfNums = nums.reduce((total, currentNum) => total + currentNum);
+
+console.log(sumOfNums);
+
+//TODO: Write a fxn. that takes an Array of Numbers and returns ... the average.
+
+function getAvg(nums) {
+  return nums.reduce((total, num) => total + num) / nums.length;
 }
 
-function double(n) {
-  return n * 2;
-}
-
-//TODO: Add 1 and double each num inside of nums only using the first 2 fxns
-console.log(nums.map(num => add1(num)).map(num => double(num)));
-
-//TODO: FILTER out all odd numbers and then repeat
-const results = nums
-  .filter(num => num % 2 === 1)
-  .map(num => add1(num))
-  .map(num => double(num));
-
-console.log(results);
-//functional programming employs function composition with pure functions
+console.log(getAvg(nums));
